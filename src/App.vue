83e0f45416d43f254a5090cfbe04cd3a3b006841
vaishali gupta <vaishaligupta.gh@gmail.com>
2022-03-23 23:24:39 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  
+
+  <ProductListPage/>
+</template>
+
+<script>
+
+import ProductListPage from './components/ProductListPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    
+    ProductListPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ProductListPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header-s">
+    
+
+    
+
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'ProductListPage',
+  props: {
+    msg: String
+  }
+}
+</script>
+<style>
+.header-s{
+  background-color: blue;
+ height: 48px;
+  width: 100%;
+}
+
+</style>
