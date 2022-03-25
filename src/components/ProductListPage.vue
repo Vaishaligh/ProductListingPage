f21d2df7adc1840856691869d004bbcbe45c4672
vaishali gupta <vaishaligupta.gh@gmail.com>
2022-03-25 15:04:09 +0530

    create header

--- src/components/ProductListPage.vue
+++ src/components/ProductListPage.vue
@@ -1,72 +1,109 @@
 <template>
-<header class="header">
-  <nav class="navbar">
-  <a href="#" class="women">WOMEN<i class="fas fa-sort-desc" aria-hidden="true"></i></a>
-  <a href="#" class="men">MEN<i class="fas fa-sort-desc" aria-hidden="true"></i></a>
-  <a href="#" class="sale">SALE<i class="fas fa-sort-desc" aria-hidden="true"></i></a>
-  <a href="#" class="collection">COLLECTION<i class="fas fa-sort-desc" aria-hidden="true"></i></a>
-  <a href="#"  class="logo"></a>
-  </nav>
-</header>
-<div class="products">
-  <div class="container">
-    <h1 class="lg-title">Women Pashima Shawls</h1>
-    <div class="product-items">
-      <div class="product">
-        <div class="product-content">
-          <div class="product-img">
-            <img src="../assets/download.jpg"/>
-          </div>
-        </div>
+<div class="header_wrapper  search_show">
+<div class="top-header-strip">
+  <div class="top-header-strip-left">
+    <p class="mb-0">
+        Biggest Online range of Pure Pashmina Shawls ! Shop Now.
+    </p>
+  </div>
+  <div class="top-header-strip-right">
+    <ul>
+      <li class="nav-item dropdown flegDrop" style="pointer-events: auto;">
+        <a href="#" class="toggle-icon pull-right">
+          <img src="../assets/indFlag.jpg"/>INR
+        </a>
+      </li>
+      <li>
+        <a href="#">Our stores </a>
+      </li>
+      <li class="logion option">
+        <a href="#"><span class="toggle-icon pull-right"></span>Login</a>
+      </li>
+    </ul>
+  </div>
+</div>
+<div class="header sticky" >
+  <nav class="navbar navbar-expand-md"> 
+    
+    <div id="myNavbar" class="">
+      <div class="mobile-menu-container">
+          <ul class="navbar-nav">
+            <li id="women1" class="nav-item dropdown" style="pointer-events: auto;">
+              <a class="nav-link font-bold dropdown_toggle" style="font-weight: bold;">WOMEN</a>
+            </li>
+            <li id="men27" class="nav-item dropdown" style="pointer-events: auto;">
+              <a class="nav-link font-bold dropdown_toggle" style="font-weight: bold;">MEN</a>
+            </li>
+             <li id="home41" class="nav-item dropdown" style="pointer-events: auto;">
+              <a class="nav-link font-bold dropdown_toggle" style="font-weight: bold;">SALE</a>
+            </li>
+             <li id="gift-cards434" class="nav-item dropdown" style="pointer-events: auto;">
+              <a class="nav-link font-bold dropdown_toggle" style="font-weight: bold;">CoLLECTION</a>
+            </li>
+          </ul>
       </div>
+    </div>
+  </nav>
+  <a class="navbar-brand desktop-logo nuxt-link-active">
+    <img src="../assets/logo.svg" alt="Ahujasons" title="ahujasons"/>
+  </a>
+  <div class="user-logins">
+    <div class="rightNav for-desktop">
+      <ul class="navbar-nav">
+        <li class="nav-item dropdown float-left">
+          <a href="" class="nav-link font-bold dropdown_toggle">OUR STORY</a>
+          </li>
+          <li class="nav-item float-left">
+          <a href="" class="nav-link font-bold">CRAFTMANSHIP</a>
+          </li>
+      </ul>
+    </div>
+    <div class="user-logins-icons">
+      <ul>
+        <li class=" flegDrop for_mobile_only">
+          <a class="">
+            <span>
+              <i aria-hidden="true" class="fa fa-heart-o"></i>
+            </span>
+            wishlist
+          </a>
+        </li>
+      </ul>
     </div>
   </div>
 </div>
+</div>
+<div class="container-fluid message-container">
+  <div id="page_loader" class="page_loader" style="display:none"></div>
+  
+</div>
+<div id="st-container-hide"></div>
+
+
 
 </template>
 
 <script>
+// import Vue from 'vue';
+// import axios from 'axios';
+// import VueAxios from 'vue-axios';
 
+// Vue.use(VueAxios, axios);
 export default {
   name: 'ProductListPage',
   props: {
     msg: String
-  }
+  },
+  // mounted() {
+  //   Vue.axios.get('https://pim.wforwoman.com/pim/pimresponse.php/?service=category&store=1&url_key=top-wear-kurtas&page=1&count=20&sort_by=&sort_dir=desc&filter=')
+  //   .then((resp)=>{
+  //     console.warn(resp)
+  //   })
+  // }
 }
 </script>
 <style>
-*{
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-  outline: none;
-  border: none;
-  text-decoration: none;
-  text-transform: capitalize;
-  transition: all .2s linear;
-}
-
-.products{
-  padding: 3.2rem 0;
-}
-.products .text-light{
-  text-align: center;
-  width: 70%;
-  margin: 0.9rem;
-}
-.product{
-  margin: 2rem;
-  position: relative;
-  background: #fff
-}
-.logo{
-background: transparent url('../assets/Group 2.png') 0% 0% no-repeat padding-box;
-opacity: 1;
-}
-.navbar{
-  background-color: blue;
-  
-}
+  @import "../assets/main.scss";
 
 
 
