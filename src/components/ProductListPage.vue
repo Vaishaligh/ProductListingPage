ad48d2135fe6eabe6a2be45d171da66f4b2bb37f
vaishali gupta <vaishaligupta.gh@gmail.com>
2022-03-27 23:32:52 +0530

    add footer

--- src/components/ProductListPage.vue
+++ src/components/ProductListPage.vue
@@ -123,7 +123,8 @@
     </nav>
   </div>
   <!--header end -->
-  <div>
+  <!--section start -->
+  <div class="">
     <div class="container-fluid">
       <div class="col-md-12">
         <div class="item">
@@ -138,7 +139,7 @@
         <div class="col-md-2 col-12">Short by</div>
       </div>
     </div>
-    <div container-fluid>
+    <div class="container-fluid">
       <section>
         <div class="row">
           <div class="col-sm-3">
@@ -305,6 +306,131 @@
       </section>
     </div>
   </div>
+  <!--section end -->
+  <!--footer start -->
+  <footer id="footer" class="section-bg">
+    <div class="footer_main_container">
+      <div class="footer_text_content"></div>
+      <div class="footerLogo">
+        <div class="footerLogoWrap">
+          <img src="../assets/flogo.png" />
+        </div>
+      </div>
+    
+    <div class="footer-links-container">
+      <div class="footer_links">
+        <div class="customer_menu link_column home-links">
+          <h2 class="">Shop</h2>
+          <ul>
+            <li class="men">
+              <a href="#">Men</a>
+            </li>
+            <li class="woman">
+              <a href="#">Woman</a>
+            </li>
+            <li class="home">
+              <a href="#">Home</a>
+            </li>
+            <li class="sale">
+              <a href="#">Sale</a>
+            </li>
+            <li class="collection">
+              <a href="#">Collection</a>
+            </li>
+          </ul>
+        </div>
+        <div class="customer_menu link_column home-links">
+          <h2 class="">QUICK LINKS</h2>
+          <ul>
+            <li class="men">
+              <a href="#">Ahujasons Export</a>
+            </li>
+            <li class="woman">
+              <a href="#">Ahujasons Wholesale</a>
+            </li>
+            <li class="home">
+              <a href="#">Home</a>
+            </li>
+            <li class="sale">
+              <a href="#">Sale</a>
+            </li>
+            <li class="collection">
+              <a href="#">Collection</a>
+            </li>
+          </ul>
+        </div>
+        <div class="customer_menu link_column home-links">
+          <h2 class="">CUSTOMER SERVICE</h2>
+          <ul>
+            <li class="men">
+              <a href="#">Terms and Condition </a>
+            </li>
+            <li class="woman">
+              <a href="#">Shipping & Delivery</a>
+            </li>
+            <li class="home">
+              <a href="#">Returns & Cancellation</a>
+            </li>
+            <li class="sale">
+              <a href="#">Contact us </a>
+            </li>
+            <li class="collection">
+              <a href="#">Store appointment</a>
+            </li>
+          </ul>
+        </div>
+        <div class="customer_menu link_column home-links">
+          <h2 class="">MY PROFILE</h2>
+          <ul>
+            <li class="men">
+              <a href="#">My Account </a>
+            </li>
+            <li class="woman">
+              <a href="#">Track Order</a>
+            </li>
+            <li class="home">
+              <a href="#">My Cart</a>
+            </li>
+            <li class="sale">
+              <a href="#">Wishlist </a>
+            </li>
+            <li class="collection">
+              <a href="#">Order History</a>
+            </li>
+          </ul>
+        </div>
+       
+      </div>
+       <div class="footer-btm-center for-dekhtop-view">
+          <div class="homepage_newsletter">
+            <div class="block newsletter">
+              <div class="title text-left">
+                <h3>SIGN UP AND SAVE</h3>
+                <strong>Subscribe to our newsletter and get 10% off your first purchase</strong>
+              </div>
+              <div class="content">
+                <form id="newsletter-validate-detail" class="form subscribe">
+                  <div class="field newsletter">
+                    <div class="control">
+                      <input name="email" type="text" id="newsletter" placeholder="Enter your email address"/>
+                    </div>
+                  </div>
+                  <div class="actions"> 
+                    <button id="Submit" title="Subscribe" class="action">
+                      <span>Subscribe</span>
+                    </button>
+                  </div>
+                </form>
+              </div>
+              <div class="socialIcons">
+                <h3>GIVE US A FOLLOW</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    </div>
+  </footer>
 </template>
 
 <script>
