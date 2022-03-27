f8a6211c6ee756a17efeab27f3cf8082916fc7ff
vaishali gupta <vaishaligupta.gh@gmail.com>
2022-03-27 18:20:19 +0530

    add card

--- src/components/ProductListPage.vue
+++ src/components/ProductListPage.vue
@@ -1,93 +1,310 @@
 <template>
-<!-- top header start -->
-<div class="top-header-strip">
-  <div class="top-header-strip-left">
-    <p>
-        COMPLIMENTARY GIFT BOX PACKING ON ALL PRE-PAID ORDERS
-    </p>
+  <!-- top header start -->
+  <div class="top-header-strip">
+    <div class="top-header-strip-left">
+      <p>COMPLIMENTARY GIFT BOX PACKING ON ALL PRE-PAID ORDERS</p>
+    </div>
+    <div class="top-header-strip-right">
+      <ul>
+        <li class="flegDrop">
+          <a href="#" class="toggle-icon pull-right"><img src="" /></a>
+        </li>
+        <li>
+          <a href="#">our stores</a>
+        </li>
+        <li>
+          <a href="#">my account</a>
+        </li>
+        <li><a href="#">blog </a></li>
+      </ul>
+    </div>
   </div>
-  <div class="top-header-strip-right">
-    <ul>
-      <li class="flegDrop">
-        <a href="#" class="toggle-icon pull-right"><img src=""/></a></li>
-      <li> 
-        <a href="#">our stores</a></li>
-      <li>
-        <a href="#">my account</a></li>
-      <li><a href="#">blog </a></li>
-    </ul>
+  <!-- top header end -->
+  <!--header start -->
+  <div class="header">
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Women
+            </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Men
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"
+              >Sale<span class="sr-only">(current)</span></a
+            >
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Collection
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <img src="../assets/lgo.png" alt="Ahujasons" title="ahujasons" />
+            </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Our Story
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"
+              ><span class="sr-only">(current)</span>CRAFTMANSHIP</a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <img src="../assets/search.png" alt="" title="" />
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <img src="../assets/wishlist.png" alt="" title="" />
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <img src="../assets/cart.png" alt="" title="" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </div>
-</div>
-<!-- top header end -->
-<!--header start -->
-<div class="header" >
- <nav class="navbar navbar-expand-lg navbar-light ">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Women
-        </a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Men
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Sale<span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Collection
-        </a>
-      </li>
-      <li class="nav-item " >
-        <a class="nav-link " href="#" >
-      <img src="../assets/lgo.png" alt="Ahujasons" title="ahujasons"/>
-     </a>
-      </li>
-       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Our Story
-        </a>
-      </li>
-      <li class="nav-item ">
-        <a class="nav-link" href="#"><span class="sr-only">(current)</span>CRAFTMANSHIP</a>
-      </li>
-      <li class="nav-item " >
-        <a class="nav-link " href="#" >
-      <img src="../assets/search.png" alt="" title=""/>
-     </a>
-      </li>
-       <li class="nav-item " >
-        <a class="nav-link " href="#" >
-      <img src="../assets/wishlist.png" alt="" title=""/>
-     </a>
-      </li>
-       <li class="nav-item " >
-        <a class="nav-link " href="#" >
-      <img src="../assets/cart.png" alt="" title=""/>
-     </a>
-      </li>
-     </ul>
-  </div>
-  </nav>
- </div>
- <!--header end -->
-<div>
-  <div class="container-fluid ">
-    <div class="col-md-12 ">
-      <div class="item">
-        <h1>Women Pashima shawls</h1>
-        <p>810 items</p>
+  <!--header end -->
+  <div>
+    <div class="container-fluid">
+      <div class="col-md-12">
+        <div class="item">
+          <h1>Women Pashima shawls</h1>
+          <p>810 items</p>
         </div>
       </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-2 col-12">Show filter</div>
+        <div class="col-md-2 col-12">Short by</div>
+      </div>
+    </div>
+    <div container-fluid>
+      <section>
+        <div class="row">
+          <div class="col-sm-3">
+            <div class="card" style="width: 18rem">
+              <img
+                class="card-img-top"
+                src="../assets/card.png"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Women Pashmina Shawl</h5>
+                <p class="card-text">Rs.150,000</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="card" style="width: 18rem">
+              <img
+                class="card-img-top"
+                src="../assets/card.png"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Women Pashmina Shawl</h5>
+                <p class="card-text">Rs.150,000</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="card" style="width: 18rem">
+              <img
+                class="card-img-top"
+                src="../assets/card.png"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Women Pashmina Shawl</h5>
+                <p class="card-text">Rs.150,000</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="card" style="width: 18rem">
+              <img
+                class="card-img-top"
+                src="../assets/card.png"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Women Pashmina Shawl</h5>
+                <p class="card-text">Rs.150,000</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-3">
+            <div class="card" style="width: 18rem">
+              <img
+                class="card-img-top"
+                src="../assets/card.png"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Women Pashmina Shawl</h5>
+                <p class="card-text">Rs.150,000</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="card" style="width: 18rem">
+              <img
+                class="card-img-top"
+                src="../assets/card.png"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Women Pashmina Shawl</h5>
+                <p class="card-text">Rs.150,000</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="card" style="width: 18rem">
+              <img
+                class="card-img-top"
+                src="../assets/card.png"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Women Pashmina Shawl</h5>
+                <p class="card-text">Rs.150,000</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="card" style="width: 18rem">
+              <img
+                class="card-img-top"
+                src="../assets/card.png"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Women Pashmina Shawl</h5>
+                <p class="card-text">Rs.150,000</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-3">
+            <div class="card" style="width: 18rem">
+              <img
+                class="card-img-top"
+                src="../assets/card.png"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Women Pashmina Shawl</h5>
+                <p class="card-text">Rs.150,000</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="card" style="width: 18rem">
+              <img
+                class="card-img-top"
+                src="../assets/card.png"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Women Pashmina Shawl</h5>
+                <p class="card-text">Rs.150,000</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="card" style="width: 18rem">
+              <img
+                class="card-img-top"
+                src="../assets/card.png"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Women Pashmina Shawl</h5>
+                <p class="card-text">Rs.150,000</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="card" style="width: 18rem">
+              <img
+                class="card-img-top"
+                src="../assets/card.png"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Women Pashmina Shawl</h5>
+                <p class="card-text">Rs.150,000</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
- </div>
 </template>
 
 <script>
@@ -98,9 +315,9 @@
 // Vue.use(VueAxios, axios);
 
 export default {
-  name: 'ProductListPage',
+  name: "ProductListPage",
   props: {
-    msg: String
+    msg: String,
   },
   // mounted() {
   //   Vue.axios.get('https://pim.wforwoman.com/pim/pimresponse.php/?service=category&store=1&url_key=top-wear-kurtas&page=1&count=20&sort_by=&sort_dir=desc&filter=')
@@ -108,10 +325,9 @@ export default {
   //     console.warn(resp)
   //   })
   // }
-}
+};
 </script>
 <style lang="css">
-@import'~bootstrap/dist/css/bootstrap.css';
+@import "~bootstrap/dist/css/bootstrap.css";
 @import "../assets/main.scss";
 </style>
-
