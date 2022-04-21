3abe809f1f2126495f8fb53b7d99a27be99eea06
vaishali gupta <vaishaligupta.gh@gmail.com>
2022-04-21 10:52:39 +0530

    add location button

--- src/components/ProductDetailPage.vue
+++ src/components/ProductDetailPage.vue
@@ -319,7 +319,32 @@
             </div>
           </div>
           <div class="other-info down-other-info">
-           
+           <div class="location">
+          <label for="pin">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              fill="#000000"
+            >
+              <path d="M0 0h24v24H0V0z" fill="none" />
+              <path
+                d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zM7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 2.88-2.88 7.19-5 9.88C9.92 16.21 7 11.85 7 9z"
+              />
+              <circle cx="12" cy="9" r="2.5" />
+            </svg>
+          </label>
+          <input
+            id="pin"
+            name="pin"
+            type="tel"
+            pattern="[0-9]*"
+            maxlength="6"
+            placeholder="Enter pincode"
+          />
+          <span>Change Pin</span>
+        </div>
           </div>
         </div>
       </div>
